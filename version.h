5670f4ed8334e6134f8d54bdf608a0ce03de7a18
Cedric ROMAN <roman@numengo@com>
2014-10-08 15:44:36 +0200

    update version

--- version.h
+++ version.h
@@ -3,12 +3,12 @@
 
 #define RC_FILENAME "NgoErr\0"
 #define RC_DESCRIPTION_STRING "library for exception handling\0"
-#define RC_FILEVERSION 1,1,0,0
-#define RC_FILEVERSION_STRING "1.1rev0\0"
+#define RC_FILEVERSION 1,2,0,0
+#define RC_FILEVERSION_STRING "1.2rev0\0"
 
 #define RC_PRODUCTNAME_STRING "numenGo|Toolbox\0"
-#define RC_PRODUCTVERSION 13,12,0,0
-#define RC_PRODUCTVERSION_STRING "13.12rev0\0"
+#define RC_PRODUCTVERSION 14,10,0,0
+#define RC_PRODUCTVERSION_STRING "14.10rev0\0"
 
 #define RC_COMPANYNAME "NUMENGO\0"
 #define RC_COPYRIGHT_STRING "Copyright (C) 2012 Numengo\0"
